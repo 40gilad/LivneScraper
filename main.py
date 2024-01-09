381d5bc2ac6f979027b8a35aad3cbe8650eee7e4
GILAD MEIR <40gilad@gmail.com>
2024-01-09 18:59:49 +0200

    just more testings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 from googlesearch import search
+from docx import Document
 
 """
 response = requests.get(
@@ -12,7 +13,7 @@ soup = BS(response.content, "html.parser")
 temp=soup.find_all('p')
 kaki=1
 """
-
+document=Document()
 def get_wiki_link(param):
 	query = f"{param} ויקי "
 	search_res=search(query, tld="co.il",stop=3)
@@ -26,11 +27,25 @@ def get_data_from_wiki(wiki_link):
 	response = requests.get(url=wiki_link)
 	soup = BS(response.content, "html.parser")
 	temp = soup.find_all('p')
+	return temp
 
 
 if __name__=='__main__':
-	wiki_link=get_wiki_link("דליה חברות אנרגיה")
+	paragraph = document.add_paragraph()
+	company_name='דליה חברות אנרגיה'
+	wiki_headline='	1. כללי: ויקיפדיה ואתר אינטרנט'
+	document.add_heading(company_name, 0)
+	document.add_heading(wiki_headline, 1)
+
+	wiki_link=get_wiki_link(company_name)
 	if wiki_link is not None:
-		get_data_from_wiki(wiki_link)
+		wiki_text=get_data_from_wiki(wiki_link)[0]
 	else:
-		print ("No Wikipedia")
+		wiki_text='NO WIKIPEDIA LINK FOUND'
+	document.add_paragraph(wiki_text)
+
+
+	document.save('test.docx')
+
+
+
